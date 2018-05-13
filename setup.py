a5b7ea58f299368ec05d192938d4ac3b87943403
yali6 <yahiaali124@gmail.com>
2018-05-13 00:55:44 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3 :: Only',
+]
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+      name='oec',
+      version='0.1.5',
+      description='API Wrapper for The Observatory for Economic Complexity',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Yahia Ali',
+      author_email='yahiaali@me.com',
+      license='MIT',
+      url='https://github.com/yahiaali/oec',
+      classifiers=classifiers,
+      install_requires=['requests'],
+      python_requires='>=3',
+)
